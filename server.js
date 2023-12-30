6e044f2bd3706658af76120162d4c29f0ccf4a39
sumanVolcanus <sumanmalakar2022@vitbhopal.ac.in>
2023-12-30 16:13:08 +0530

    adding env file

--- server.js
+++ server.js
@@ -3,25 +3,27 @@ import mongoose from "mongoose";
 import bodyparser from "express";
 import contactRouter from "./routes/contact.js";
 import cors from 'cors'
+import {config }from 'dotenv'
 
 const app = express();
+
+config({
+  path:'.env'
+})
 
 app.use(bodyparser.json());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.Frontend_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
 );
-
+ 
 mongoose
-  .connect(
-    "mongodb+srv://sumanmalakar2022:G7VwOsxTULrhE9SR@volcanus.tkilonf.mongodb.net/",
-    {
-      dbName: "MERN_Project_Contact_keeper",
-    }
-  )
+  .connect(process.env.Mongo_URL, {
+    dbName: "MERN_Project_Contact_keeper",
+  })
   .then(() => console.log("MongoDB Connected..!"));
 
 // contact router
@@ -31,5 +33,5 @@ app.get("/", (req, res) => {
   res.json({ message: "This is home route" });
 });
 
-const port = 3000;
+const port = process.env.Port;
 app.listen(port, () => console.log(`server is running on port ${port}`));
